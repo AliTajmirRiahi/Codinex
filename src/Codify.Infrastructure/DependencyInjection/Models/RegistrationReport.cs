6bd7cf5e10135ee307b6338b5d2de0abb951179b
Ali Tajmir Riahi <riahi.tajmir@gmail.com>
2026-07-27 17:14:14 +0330

    Add DI registration reporting

---
+++ src/Codify.Infrastructure/DependencyInjection/Models/RegistrationReport.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Codify.Infrastructure.DependencyInjection.Models
+{
+    public sealed class RegistrationReport
+    {
+        public IReadOnlyList<RegistrationItem> Items { get; }
+    }
+}
