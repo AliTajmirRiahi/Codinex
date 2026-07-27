d923a30365f186685c2cf89657283b805026b405
Ali Tajmir Riahi <riahi.tajmir@gmail.com>
2026-07-27 17:10:57 +0330

    Add AutoRegisterAttribute infrastructure

---
+++ src/Codify.Infrastructure/DependencyInjection/Attributes/AutoRegisterAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Codify.Infrastructure.DependencyInjection.Attributes;
+
+[AttributeUsage(AttributeTargets.Class)]
+public sealed class AutoRegisterAttribute(
+    ModuleType module,
+    ServiceLifetime lifetime = ServiceLifetime.Singleton)
+    : Attribute
+{
+    public ServiceLifetime Lifetime { get; } = lifetime;
+
+    public ModuleType Module { get; } = module;
+}
